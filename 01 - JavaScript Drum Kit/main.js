71dccc62011e6bdde15205574318287d511ce3b5
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-19 16:43:49 +0900

    completed

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,20 +1,20 @@
-const keys = document.querySelectorAll(".key");
-
+// Listening keydown of all keys
 window.addEventListener("keydown", e => {
   playAudio(e);
   generateEffect(e);
 });
 
-// Removing style after event occured
-keys.forEach(key => {
+// Listening keys in html for transitionend
+document.querySelectorAll(".key").forEach(key => {
   key.addEventListener("transitionend", e => {
-    key.classList.remove("playing");
+    removeEffect(e);
   });
 });
 
 // Playing audio when event occurs
 const playAudio = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
   audio.currentTime = 0; //to get audio instantly
   audio.play();
 };
@@ -23,4 +23,10 @@ const playAudio = e => {
 const generateEffect = e => {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   key.classList.add("playing");
+};
+
+// Removing style after event occured
+const removeEffect = e => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
 };
