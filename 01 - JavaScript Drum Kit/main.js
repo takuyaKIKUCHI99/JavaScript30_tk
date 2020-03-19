b36dc017730a17277a9a7930b0c6769062d8569e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-19 16:32:32 +0900

    effect remove added

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -5,12 +5,21 @@ window.addEventListener("keydown", e => {
   generateEffect(e);
 });
 
+// Removing style after event occured
+keys.forEach(key => {
+  key.addEventListener("transitionend", e => {
+    key.classList.remove("playing");
+  });
+});
+
+// Playing audio when event occurs
 const playAudio = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   audio.currentTime = 0; //to get audio instantly
   audio.play();
 };
 
+// Adding style when event occurs
 const generateEffect = e => {
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
   key.classList.add("playing");
