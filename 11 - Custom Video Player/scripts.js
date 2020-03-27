39a1004fc28641ecabae409b0b49486182f2ea92
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-03-27 12:42:37 +0900

    skip functin added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,9 +1,11 @@
 // DOM
 const player = document.querySelector(".player");
 const video = document.querySelector("video");
-const playButton = document.querySelector(".player__button");
+const playButton = document.querySelector(".toggle");
 const progress = document.querySelector(".progress");
 const progressFilled = document.querySelector(".progress__filled");
+const sliders = document.querySelectorAll(".player__slider");
+const skipButtons = document.querySelectorAll(".player__button[data-skip]");
 
 // Playing or stopping video
 function playControl(e) {
@@ -21,9 +23,17 @@ function updateProgress() {
   progressFilled.style.flexBasis = `${percentage}%`;
 }
 
-function skipProgress(e) {
+function jumpProgress(e) {
   const skippingTo = video.duration * (e.offsetX / this.offsetWidth);
   video.currentTime = skippingTo;
+}
+
+function sliderEffect() {
+  video[this.name] = this.value;
+}
+
+function skipProgress() {
+  video.currentTime += parseInt(this.dataset.skip);
 }
 
 // Eventlistner
@@ -31,4 +41,12 @@ video.addEventListener("click", playControl);
 playButton.addEventListener("click", playControl);
 
 video.addEventListener("timeupdate", updateProgress);
-progress.addEventListener("click", skipProgress);
+progress.addEventListener("click", jumpProgress);
+
+sliders.forEach(slider => {
+  slider.addEventListener("click", sliderEffect);
+});
+
+skipButtons.forEach(skip => {
+  skip.addEventListener("click", skipProgress);
+});
